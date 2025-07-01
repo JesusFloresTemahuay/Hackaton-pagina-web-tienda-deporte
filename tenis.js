3c2b01d96642232c076ecc7b2e64596505d73127
jesusflorestemahuay <jflorest@gmail.com>
2025-06-30 19:11:29 -0600

    guardando cambios

---
+++ tenis.js
@@ -0,0 +1,23 @@
+import { productos } from './productos.js';
+
+function cargaTenis(){
+   /**
+     console.log("cargado tenis");
+    let idc = "tenis0X-marca";
+    let id = idc;
+
+    for (let index = 0; index < 4; index++) {
+        id = id.replace("X", "" + index + 17);
+        document.getElementById(id).innerText = productos[index].marca + " - " + productos[17].nombre;
+        document.getElementById(id).innerText = productos[index].imagenes[0];
+        document.getElementById(id).innerText = productos[index].precio;
+        document.getElementById(id).innerText = productos[index].descripcion;
+        console.log(id + " cargado");
+        id = idc;
+    }
+    */
+}
+
+//document.getElementById("tenis01-marca").innerText = productos[17].marca + " - " + productos[17].nombre;
+document.getElementById("tenis01-marca").innerText = "sdfafdsafdsaf";
+console.log("Hola");
